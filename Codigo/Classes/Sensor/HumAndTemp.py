7a49dfbea4f0553e74f5fd1dc4810a1e50a21b2a
Guido <glambertucci@itba.edu.ar>
2021-08-29 20:53:07 -0300

    Adafruit

--- Codigo/Classes/Sensor/HumAndTemp.py
+++ Codigo/Classes/Sensor/HumAndTemp.py
@@ -1,3 +1,13 @@
+import board
+import adafruit_dht
+dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False)
+#The D4 is the pin
+
+# Initial the dht device, with data pin connected to:
+# dhtDevice = adafruit_dht.DHT22(board.D18)
+# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
+# This may be necessary on a Linux single board computer like the Raspberry Pi,
+# but it will not work in CircuitPython.
 class HumAndTemp:
     def __init__(self):
         print("Humidity and Temperature class initialized!")
@@ -5,10 +15,19 @@ class HumAndTemp:
         self.Temperature = 20
 
     def get_measurements(self):
-        self.Humidity += 1
-        self.Temperature += 1
+        try:
+            self.Humidity = dhtDevice.humidity
+            self.Temperature = dhtDevice.temperature
+            print(
+                "Temp: {:.1f} C    Humidity: {}% ".format(
+                    self.Temperature, self.Humidity
+                )
+            )
+        except RuntimeError as error:
+           print(error.args[0])
         return [self.Humidity, self.Temperature]#First humidity the second Temperature
 
     def destroy(self):
         print("Humidity and Temperature destroyed!")
+        dhtDevice.exit()
 
